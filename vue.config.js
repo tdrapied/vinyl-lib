87e514278e22077716d079efb01b090546ad8b7d
tdrapied <t.drapied@gmail.com>
2022-12-16 15:32:08 +0100

    Change icons + add pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "Vinylib",
+    themeColor: "#7688e8",
+    msTileColor: "#7688e8",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxOptions: {
+      exclude: [".htaccess"],
+    },
+  },
 });
